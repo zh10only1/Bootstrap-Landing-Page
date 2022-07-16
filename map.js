11936745cfb21348b18c1cc31298942b6ef6d23d
zh10only1 <mzainhassanbaloch@gmail.com>
2022-07-16 19:25:05 +0500

    initial commit

---
+++ map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiemgxMG9ubHkxIiwiYSI6ImNsNWd0enhlNjAxbDMzY3AydDlyaHJ3dGMifQ.mnIZjsbhjU0CwVCodv1c5A";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [73.0791, 31.4187],
+  zoom: 18,
+});
